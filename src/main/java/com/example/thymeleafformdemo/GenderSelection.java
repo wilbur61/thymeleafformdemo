bf17a9798c0f22fb81d0472ccf4eea5ca3cd94b3
wilbur61 <wroales@perscholas.org>
2024-02-05 14:41:05 -0500

    Initial commit  added radio button 2nd try

---
+++ src/main/java/com/example/thymeleafformdemo/GenderSelection.java
@@ -0,0 +1,16 @@
+package com.example.thymeleafformdemo;
+
+
+public class GenderSelection {
+    private String gender;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // Getters and setters for the 'gender' attribute
+}
