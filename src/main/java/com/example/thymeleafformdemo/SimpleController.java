0d5eda20f5dbcc2aabcf43803c2ab63410f3748d
wilbur61 <wroales@perscholas.org>
2024-02-08 14:37:30 -0500

    added radio buttons

--- src/main/java/com/example/thymeleafformdemo/SimpleController.java
+++ src/main/java/com/example/thymeleafformdemo/SimpleController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SimpleController {
@@ -25,5 +26,23 @@ public class SimpleController {
 	    	model.addAttribute("mylogin", mylogin);
 	        return "login-result";
 	    }
+	@ModelAttribute
+	public GenderSelection selection() {
+		return new GenderSelection(); // Initialize with default gender (optional)
+	}
 
+	@GetMapping("/radio-buttons")
+	public String radioButtons(Model model) {
+		GenderSelection selection = new GenderSelection();
+		selection.setGender("Male");
+		model.addAttribute("selection", selection);
+		return "radio-buttons";
+	}
+	@PostMapping("/radio-selection")
+	public String radioSelection(@RequestParam String gender, Model model) {
+		System.out.println("===>> Selected gender: " + gender);
+		// ... Add any additional logic or form processing here
+		model.addAttribute("selectedGender", gender);
+		return "radio-selection"; // Redirect or return view name
+	}
 }
